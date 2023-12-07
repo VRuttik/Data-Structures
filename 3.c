32ca206e6e299a607e73ca3ffa3602605e74cea2
Vruttik Moragha <97348689+VRuttik@users.noreply.github.com>
2023-12-07 10:05:44 -0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,24 @@
+/* Write a program to count the number of vovels in a text. */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char text[100];
+    int vowelsCount = 0;
+
+    printf("Enter the string: ");
+    scanf("%s", text);
+
+    for (int i = 0; text[i] != '\0'; ++i) {
+        char currentChar = tolower(text[i]);
+
+        if (currentChar == 'a' || currentChar == 'e' || currentChar == 'i' || currentChar == 'o' || currentChar == 'u') {
+            vowelsCount++;
+        }
+    }
+
+    printf("Number of vowels in the text: %d\n", vowelsCount);
+
+    return 0;
+}
